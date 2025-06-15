139dcdd8ed294cd0b4e3a66696f7c89b367ad56f
scottsmith0294 <dayojohn160@gmail.com>
2025-06-15 18:40:45 +0100

    new feature

---
+++ fold/home.js
@@ -0,0 +1,59 @@
+
+document.getElementById('studentName').addEventListener('input', function() {
+    const inputText = document.getElementById('result');
+    // Limit the input to a maximum of 20 characters
+    const maxLength = 10;
+    if (this.value.length > maxLength) {
+        this.value = this.value.slice(0, maxLength);
+        inputText.textContent = "Name cannot exceed 20 characters.";
+    }
+});
+const calculateAverage = ()=>{
+    
+    // Get the student name and scores from the input fields
+const studentName = document.getElementById('studentName').value || "Student";
+const science = parseFloat(document.querySelector('#science').value)
+const maths = parseFloat(document.querySelector('#maths').value)
+const english = parseFloat(document.querySelector('#english').value)
+const biology = parseFloat(document.querySelector('#biology').value)
+const displayResult = document.querySelector('#result')
+const averageScore = (science + maths + english + biology) / 3;
+
+    if(averageScore >= 90) {
+        displayResult.textContent =`${studentName} You have an A grade. with the score of ${averageScore.toFixed(2)};`
+    } else if(averageScore >= 80) {
+        displayResult.textContent =` ${studentName} You have  B grade. with the score of ${averageScore.toFixed(2)}`
+    } else if(averageScore >= 70) {
+        displayResult.textContent = `${studentName} You have  C grade. with the score of ${ averageScore.toFixed(2)}`;
+    } else if(averageScore >= 60) {
+        displayResult.textContent = `${studentName} You have  D grade. with the score of ${averageScore.toFixed(2)}`;
+    } else {
+        displayResult.textContent = studentName + ` failed. with the score of ${averageScore.toFixed(2)} please work harder next time.`;
+    }
+    if(isNaN(science) || isNaN(maths) || isNaN(english) || isNaN(biology)) {
+        displayResult.textContent = "Please enter valid numbers for all subjects.";
+    }   
+    
+// Clear the input fields after calculation
+    document.getElementById('studentName').value = ''
+    document.getElementById('science').value = ''
+    document.getElementById('maths').value = ''
+    document.getElementById('english').value = ''
+    document.getElementById('biology').value = ''
+
+}
+
+const randomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+
+    }
+    return   document.body.style.backgroundColor = color;
+  
+}
+setInterval(() => {
+    randomColor()
+}, 20000);
+    
